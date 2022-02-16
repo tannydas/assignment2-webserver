3165bb854e5c635463b15f4e81bc9b75bf943b18
tannydas <79417918+tannydas@users.noreply.github.com>
2022-02-16 11:38:46 -0500

    Got rid of errors

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-# import socket module
+#import socket module
 from socket import *
 # In order to terminate the program
 import sys
@@ -7,22 +7,27 @@ import sys
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
+  port = 13331
   serverSocket.bind(("", port))
   #Fill in start
-
+  serverSocket.listen(1)
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()  #Fill in start
+
+
+    # #Fill in end
     try:
 
       try:
-        message = #Fill in start    #Fill in end test
+        message = '404 Not Found' #Fill in start    #Fill in end test
+        print(message)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = '' #Fill in start     #Fill in end
         
         #Send one HTTP header line into socket.
         #Fill in start
@@ -35,13 +40,13 @@ def webServer(port=13331):
 
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
-      except IOError:
+      except IOError: return
         # Send response message for file not found (404)
         #Fill in start
 
         #Fill in end
 
-
+      connectionSocket.close()
         #Close client socket
         #Fill in start
 
